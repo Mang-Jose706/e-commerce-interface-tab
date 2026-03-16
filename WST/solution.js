da3e9cdbd2cbe3565eca7e0ba4f00edb2b6c5712
Jose Manuel Cardeño <jmcardeno706@gmail.com>
2026-03-16 23:26:31 +0800

    Problem 5

--- WST/solution.js
+++ WST/solution.js
@@ -1,19 +1,12 @@
-const players = [
-  { name: "Joban", score: 10 },
-  { name: "Kent", score: 5 },
-  { name: "Droods", score: 12 },
-  { name: "Jols", score: 7 },
-  { name: "Jose", score: 9 },
-  { name: "Jeanne", score: 4 },
-  { name: "Shem", score: 11 },
-  { name: "Mark", score: 8 },
-  { name: "Arce", score: 15 },
-  { name: "Ambasing", score: 6 }
-];
+class Item {
+  #discount = 0.1;
 
-function getTopScorers(playerList) {
-  return playerList
-    .filter(player => player.score > 8)
-    .map(player => player.name)
-    .join(", ");
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  get finalPrice() {
+    return this.price * (1 - this.#discount);
+  }
 }
