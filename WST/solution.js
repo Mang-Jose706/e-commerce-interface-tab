c40946f9f846f3418d2a55c7f0b5b38e67f85113
Jose Manuel Cardeño <jmcardeno706@gmail.com>
2026-03-16 23:29:59 +0800

    Problem 6

--- WST/solution.js
+++ WST/solution.js
@@ -1,12 +1,12 @@
-class Item {
-  #discount = 0.1;
-
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
-  }
-
-  get finalPrice() {
-    return this.price * (1 - this.#discount);
+function safeDivide(a, b) {
+  try {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+  } catch (error) {
+    return error.message;
+  } finally {
+    console.log("Operation attempted");
   }
 }
