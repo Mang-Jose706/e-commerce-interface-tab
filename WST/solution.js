255e4459ca89853fef159a0a2c909b580c7edcb1
Jose Manuel Cardeño <jmcardeno706@gmail.com>
2026-03-16 23:23:15 +0800

    Problem 4

--- WST/solution.js
+++ WST/solution.js
@@ -1,8 +1,19 @@
-function calculateTotal(...numbers) {
-  return numbers.reduce((sum, current) => {
-    if (typeof current !== 'number' || Number.isNaN(current)) {
-      throw new TypeError("Invalid input: All arguments must be numbers");
-    }
-    return sum + current;
-  }, 0);
+const players = [
+  { name: "Joban", score: 10 },
+  { name: "Kent", score: 5 },
+  { name: "Droods", score: 12 },
+  { name: "Jols", score: 7 },
+  { name: "Jose", score: 9 },
+  { name: "Jeanne", score: 4 },
+  { name: "Shem", score: 11 },
+  { name: "Mark", score: 8 },
+  { name: "Arce", score: 15 },
+  { name: "Ambasing", score: 6 }
+];
+
+function getTopScorers(playerList) {
+  return playerList
+    .filter(player => player.score > 8)
+    .map(player => player.name)
+    .join(", ");
 }
